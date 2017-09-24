558cae1c85f6d62bbb772da6e6c9fe20b8475629
nguyen duc dai <nguyenducdaiictu@gmail.com>
2017-09-24 08:11:23 +0700

    up

--- Portal/Portal/wwwroot/js/applications/common.js
+++ Portal/Portal/wwwroot/js/applications/common.js
@@ -25,5 +25,27 @@
 });
 
 function renderDatatable() {
-    $('#dataTable').DataTable();
+   $('#dataTable').DataTable();
+}
+
+function MessageEvent(msg, idisplay, typeError) {
+    var html = "";
+    if (typeError == 's') {
+        html += '<div class="alert alert-success alert-dismissible" >';
+        html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+        html += '<strong>[success!]</strong> ' + msg;
+        html += '</div>';
+    } else if (typeError == 'w') {
+        html += '< div class="alert alert-warning alert-dismissible" >';
+        html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+        html += '<strong>[warning]</strong> ' + msg;
+        html += '</div>';
+    } else {
+        html += '< div class="alert alert-danger alert-dismissible" >';
+        html += '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+        html += '<strong>[error]</strong> ' + msg;
+        html += '</div>';
+    }
+    $(idisplay).css({ "display": "block" });
+    $(idisplay).html(html);
 }
