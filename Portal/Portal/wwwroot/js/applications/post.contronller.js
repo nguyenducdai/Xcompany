f95d138d46882e6bd6eedf54f6f6df2a18455521
quangdai <quangdai.ictu@gmail.com>
2017-09-26 01:53:47 +0700

    ok

--- Portal/Portal/wwwroot/js/applications/post.contronller.js
+++ Portal/Portal/wwwroot/js/applications/post.contronller.js
@@ -1,5 +1,79 @@
-﻿app.controller("postContronller", ["$scope", "$rootScope", "$http", function ($scope, $rootScope,$http) {
+﻿app.controller("PostController", ["$scope", "$rootScope", "$http", function ($scope, $rootScope, $http) {
 
+    $scope.DataTable = {};
+    $rootScope.post = {};
 
+    $scope.showDialog = function (id) {
+        $("#myModal").modal();
+        if (id == undefined) {
+            //$rootScope.post = {
+            //    status: true,
+            //    createAt: null,
+            //    updateAt: null
+            //}
+            $rootScope.Tilte = "Add New Post";
+
+        } else {
+            // EDIT DATA
+            $rootScope.Tilte = "Update Post";
+            $http.get("/api/PostsAPI/" + id).then(function (response) {
+                $rootScope.post = response.data;
+            }, function (error) {
+                console.log(error);
+            });
+
+        }
+    }
+
+    // post -> Add , get->get , put->update . delete-> del
+
+    $scope.delete = function (id) {
+        if (confirm("do you want delete ?")) {
+            $http.delete("/api/PostsAPI/" + id).then(function (response) {
+                $scope.LoadData();
+                MessageEvent("delete done !", "#messageinfo", "s")
+            }, function (error) {
+                console.log(error);
+            });
+        }
+    }
+
+    // SAVE POST / PUT
+    $rootScope.save = function (id) {
+        // SAVE ADD
+        if (id == undefined) {
+            $rootScope.post.createAt = new Date();
+            $http.post("/api/PostsAPI/", $rootScope.post).then(function (response) {
+                $scope.LoadData();
+            }, function (error) {
+                console.log(error);
+            });
+        } else {
+            // SAVE UPDATE
+            $rootScope.post.updateAt = new Date();
+            $http.put("/api/PostsAPI/" + id, $rootScope.post).then(function (response) {
+                $scope.LoadData();
+            }, function (error) {
+                console.log(error);
+            });
+        }
+
+    }
+    //LOAD DATATABLE
+    $scope.LoadData = function () {
+        $.ajax({
+            url: "/api/PostsAPI/",
+            type: "get",
+            dateType: "json",
+            async: false,
+            cache: true,
+            success: function (response) {
+                $scope.DataTable = response;
+                //console.log($scope.DataTable);
+            }
+        });
+    }
+
+    $scope.LoadData();
 
 }]);
